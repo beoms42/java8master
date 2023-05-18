beb871e60178fa194d74cabda20c7847bd7857ec
yicho <yicho@spectra.co.kr>
2023-05-18 13:13:11 +0900

    1번 commit

--- Stream.java
+++ Stream.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Stream {
 
@@ -9,6 +10,23 @@ public class Stream {
 
         //문제 1.1
         //위와 같은 데이터를 조회하여 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
+
+        /**
+         * 예상답안
+         * 축구 : 3명
+         * 당구 : 3명
+         * 등등
+         */
+
+        Map<String, Integer> collect = userList.stream()
+                // 공백을 제거해주고
+                .map(line -> line.get(1).replaceAll("\\s", ""))
+                // flatMap으로 List<String>을 큰 String으로 만듬
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+                //hobby -> hobby : 맵의 key값, hobby는 value값, value값이 같으면 전값 + 새 밸류값
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+
+        System.out.println("collect = " + collect);
 
 
         //문제 1.2
