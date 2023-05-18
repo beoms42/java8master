ae44edff239abaa2847b0bc1d9d9a1bbc56125dd
yicho <yicho@spectra.co.kr>
2023-05-18 10:59:53 +0900

    stream 연습문제 초기commit

---
+++ Stream.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Stream {
+
+    public static void main(String[] args) {
+
+        List<List<String>> userList = new CSVReader().readCSV();
+
+        //문제 1.1
+        //위와 같은 데이터를 조회하여 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
+
+
+        //문제 1.2
+        //위와 같은 데이터를 조회하여 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
+
+
+        //문제 1.3
+        //위와 같은 데이터를 조회하여 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
+    }
+
+}
+
